45e70df00989c97aacc23913ee743143336bf724
je-swy <kateryna.shemonaieva@gmail.com>
2025-12-01 16:14:55 +0200

    Reset main to empty except README.md

--- src/components/Header/components/Logo/Logo.tsx
+++
@@ -1,7 +0,0 @@
-import logoAsset from '../../../../assets/images/logo.svg';
-
-import './Logo.css';
-
-const Logo = () => <img src={logoAsset} alt="App Logo" className='logo'/>;
-
-export default Logo;
--- src/components/PrivateRoute/PrivateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute: FC<{ children: React.ReactNode }> = ({ children }) => {
-  const token = localStorage.getItem('token');
-  return token ? <>{children}</> : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoute;
